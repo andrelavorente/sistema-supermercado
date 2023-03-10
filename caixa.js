ead6b71711949949b702900eb411dfceb5f7c6d9
André Lavorente <lavorente.a@gmail.com>
2023-03-10 14:58:05 -0300

    sistema-mercado
    Sistema de supermercado construído em JS com POO, sendo descontos individualizados dependendo do tipo do produto.

---
+++ caixa.js
@@ -0,0 +1,79 @@
+import Carrinho from "./carrinho.js"
+import Produto from "./Produtos/produto.js"
+import Carro from "./Produtos/carro.js"
+import Limpeza from "./Produtos/alimentacao.js"
+import Alimentacao from "./Produtos/alimentacao.js"
+
+const nomeProduto = document.querySelector("#nomeProd");
+const valorProduto = document.querySelector("#valorProd");
+const tipoProduto = document.querySelector("#tipoProd");
+const codigoProduto = document.querySelector("#codigoProd");
+const dataProduto = document.querySelector("#dataProd");
+const botaoAdd = document.querySelector("#adiconarProd");
+const botaoRemove = document.querySelector("#removaProd");
+const botaoDesconto = document.querySelector("#aplicaDesconto");
+const valorTotal = document.querySelector("#valorTotal");
+const valorFinal = document.querySelector("#valorFinal");
+const listaProd = document.querySelector(".lista_prod");
+const carrinho = new Carrinho();
+
+
+botaoAdd.addEventListener('click', () => adicionaProduto());
+botaoRemove.addEventListener('click', () => removerProduto());
+botaoDesconto.addEventListener('click', () => aplicarDesconto());
+tipoProduto.addEventListener('input',() => flagDataValidade());
+
+function criaProduto(){
+    let produto;
+    if(tipoProduto.value === "alimentacao"){
+        produto = new Alimentacao(nomeProduto.value, Number(valorProduto.value), codigoProduto.value, tipoProduto.value, dataProduto.value)
+    }else if(tipoProduto.value === "carro") {
+        produto = new Carro(nomeProduto.value, Number(valorProduto.value), codigoProduto.value, tipoProduto.value)
+    }else if(tipoProduto.value === "limpeza"){
+        produto = new Limpeza (nomeProduto.value, Number(valorProduto.value), codigoProduto.value, tipoProduto.value)
+    }else if (tipoProduto.value === "outro"){
+        produto = new Produto(nomeProduto.value, Number(valorProduto.value), codigoProduto.value, tipoProduto.value)
+    }   
+    return produto;
+}
+
+function adicionaProduto(){
+    const produto = criaProduto();
+    carrinho.adicionarProduto(produto);
+    exibeProdutos();
+    atualizaValorTotal();
+    console.log(produto)
+}
+
+function removerProduto(){
+    carrinho.removerProduto();
+    exibeProdutos();
+    atualizaValorTotal();
+}
+
+
+function atualizaValorTotal(){
+    valorTotal.textContent = `Valor Total: R$ ${carrinho.valor}`
+}
+
+function aplicarDesconto(){
+    valorFinal.textContent = `Valor Total: R$ ${carrinho.calculaValorFinal()}`
+}
+
+function exibeProdutos(){
+    let saida = "";
+    carrinho.listaDeProdutos.forEach((produto) => {
+        saida +=`<div class="produto"><p >${produto.nome}</p><p>${produto.tipo}</p><p>${produto.valor}</p></div>`
+    });
+    listaProd.innerHTML = saida;
+}
+
+function flagDataValidade(){
+    if(document.querySelector("#tipoProd").value === "alimentacao"){
+        document.querySelector("#dataProd").style.display = "block";
+    }else{
+        document.querySelector("#dataProd").style.display = "none";
+    }
+}
+
+
